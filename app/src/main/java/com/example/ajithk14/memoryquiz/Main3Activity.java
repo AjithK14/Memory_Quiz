145986011da2d211fe336f5eb00d390f32af36da
AjithK14 <kemisettia@gmail.com>
2018-01-09 00:37:29 -0500

    Still unfinished but.. started to integrate database in a more feasible way

--- app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
+++ app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
@@ -1,14 +1,27 @@
 package com.example.ajithk14.memoryquiz;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class Main3Activity extends AppCompatActivity {
 
+    EditText ename;
+    Button btnChoose;
+    ImageView imgView;
+    final int REQUEST_CODE_GALLERY=999;
+
+    public static SQLiteHelper sqlitehelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +39,14 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        sqlitehelper = new SQLiteHelper(this, "FacesDB.sqlite",null,1);
+        sqlitehelper.queryData("CREATE TABLE IF NOT EXISTS FACE (Id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, image BLOG)");
+
+    }
+    public void onClick(View view)
+    {
+        ActivityCompat.requestPermissions(Main3Activity.this,
+                new String[]{READ_EXTERNAL_STORAGE},REQUEST_CODE_GALLERY);
     }
 
 }
