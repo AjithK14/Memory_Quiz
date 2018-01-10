6f8bee6b165c764643f710c935fda09b2abdd471
AjithK14 <kemisettia@gmail.com>
2018-01-09 22:17:19 -0500

    Finished camera roll adding

--- app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
+++ app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
@@ -1,7 +1,15 @@
 package com.example.ajithk14.memoryquiz;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import com.example.ajithk14.memoryquiz.R;
+
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -11,16 +19,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class Main3Activity extends AppCompatActivity {
-
-    EditText ename;
-    Button btnChoose;
-    ImageView imgView;
     final int REQUEST_CODE_GALLERY=999;
-
+    ImageView imageView;
     public static SQLiteHelper sqlitehelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,8 @@ public class Main3Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +56,43 @@ public class Main3Activity extends AppCompatActivity {
     public void onClick(View view)
     {
         ActivityCompat.requestPermissions(Main3Activity.this,
-                new String[]{READ_EXTERNAL_STORAGE},REQUEST_CODE_GALLERY);
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE_GALLERY);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==REQUEST_CODE_GALLERY){
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"No permission to access folder",Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==REQUEST_CODE_GALLERY && resultCode==RESULT_OK && data != null)
+        {
+            Uri uri = data.getData();
+            try{
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+
+            }
+            catch(FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
