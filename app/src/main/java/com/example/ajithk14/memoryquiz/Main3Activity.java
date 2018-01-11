efcbc01e412209996922167ee85a60949cc51b43
AjithK14 <kemisettia@gmail.com>
2018-01-10 19:41:29 -0500

    Added option to save image to storage

--- app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
+++ app/src/main/java/com/example/ajithk14/memoryquiz/Main3Activity.java
@@ -1,19 +1,25 @@
 package com.example.ajithk14.memoryquiz;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -23,16 +29,27 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class Main3Activity extends AppCompatActivity {
     final int REQUEST_CODE_GALLERY=999;
+    private Context TheThis;
+    public Uri imageUri;
+    private Bitmap takenImage;
     static final int REQUEST_IMAGE_CAPTURE=1;
+    static final int REQUEST_TAKE_PHOTO = 1;
     ImageView imageView;
     public static SQLiteHelper sqlitehelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +76,12 @@ public class Main3Activity extends AppCompatActivity {
     {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (intent.resolveActivity(getPackageManager()) != null){
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, "New Picture");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "From your Camera");
+            imageUri = getContentResolver().insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);}
     }
     public void onClick(View view)
@@ -69,16 +92,13 @@ public class Main3Activity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode==REQUEST_CODE_GALLERY){
-            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == REQUEST_CODE_GALLERY) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
                 startActivityForResult(intent, REQUEST_CODE_GALLERY);
-            }
-            else
-            {
-                Toast.makeText(getApplicationContext(),"No permission to access folder",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "No permission to access folder", Toast.LENGTH_SHORT).show();
             }
             return;
         }
@@ -110,8 +130,15 @@ public class Main3Activity extends AppCompatActivity {
                 finish();
             }
             else{
-            Bitmap takenImage = (Bitmap)data.getExtras().get("data");
-            imageView.setImageBitmap(takenImage);
+                try{
+                takenImage = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), imageUri);
+                String imageurl = getRealPathFromURI(imageUri);
+                imageView.setImageBitmap(takenImage);}
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
             builder.setTitle("Storage");
@@ -121,9 +148,8 @@ public class Main3Activity extends AppCompatActivity {
             builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
 
                 public void onClick(DialogInterface dialog, int which) {
-                    // Do nothing but close the dialog
+                    MediaStore.Images.Media.insertImage(getContentResolver(), takenImage, "Img1" , "Taken");
 
-                    dialog.dismiss();
                 }
             });
 
@@ -142,4 +168,13 @@ public class Main3Activity extends AppCompatActivity {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
 }
